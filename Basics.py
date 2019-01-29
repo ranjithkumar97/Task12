bf4744a9cc4906bb26aef27e8e7a60d6e7dab00c
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-29 21:01:49 +0530

    Basics of python

---
+++ Basics.py
@@ -0,0 +1,17 @@
+a="hello_world"
+print(a[0,2,3,4,5,6])
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+x = lambda a, b : a * b
+print(x(5, 6))
+car1 = "Ford";
+car2 = "Volvo";
+car3 = "BMW";
+print(car1)
